8b6a6e0dbaf9c31f03eb48847d5b985b4762d5dd
JiHyeon Yun <heon__y@naver.com>
2024-02-13 18:42:25 +0900

    main

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -3,6 +3,21 @@ package hello.core.member;
 public class Member {
     private Long id;
     private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -18,21 +33,5 @@ public class Member {
 
     public void setGrade(Grade grade) {
         this.grade = grade;
-    }
-
-    private Grade grade;
-
-    public Member(Long id, String name, Grade grade) {
-        this.id = id;
-        this.name = name;
-        this.grade = grade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 }
